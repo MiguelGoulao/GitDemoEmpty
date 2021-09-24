d99e3eb6b70b2bea737fd648bea1eaa0270d5457
Miguel Goulao <mgoul@fct.unl.pt>
2021-09-24 12:25:54 +0100

    Added a second line to the messages

--- GitDemoProjectEclipse/src/GitTest.java
+++ GitDemoProjectEclipse/src/GitTest.java
@@ -3,6 +3,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Git is not fun with Eclipse");
+		System.out.println("Git is super fun with Eclipse");
 	}
 
 }
